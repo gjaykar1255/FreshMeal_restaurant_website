9c2b992867d89edbaafda439e049874df34adc16
gjaykar1255 <95492981+gjaykar1255@users.noreply.github.com>
2021-12-04 14:07:37 +0530

    Add files via upload

---
+++ JavascriptFiles/app.js
@@ -0,0 +1,6 @@
+function fn1(element){
+    document.querySelector(".x123").style.outline='none';
+ };
+ document.querySelector(".fa-bars").addEventListener("click",function(){
+     document.querySelector(".site-content-wrapper").classList.toggle("scaled");
+ });
